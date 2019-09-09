19e1bb0adc063b43292e324aebf8588a453064b1
Saroj Lamichhane <sarojlamichhane@Sarojs-MBP.attlocal.net>
2019-09-08 19:26:38 -0500

    second commit

---
+++ linkedlist2.py
@@ -0,0 +1,38 @@
+# This is a python program to find the size of a singly linked list
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class linkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+
+    def iterateItem(self):
+        current = self.tail
+        while current:
+            value = current.data
+            current = current.next
+            yield value
+    def apprendItem(self, data):
+        node = Node(data)
+        if self.head:
+            self.head.next = node
+            self.head = node
+        else:
+            self.tail = node
+            self.head = node
+        self.count += 1
+
+item = linkedList()
+item.apprendItem("Stanford University")
+item.apprendItem("Harvard University")
+item.apprendItem("Boston University")
+
+for i in item.iterateItem():
+    print(i)
+
+print("The size of the linked list is ",item.count)
