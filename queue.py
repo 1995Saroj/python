67867a277261a2cbf552d359e79174f86f450a57
Saroj Lamichhane <sarojlamichhane@Sarojs-MBP.attlocal.net>
2019-09-14 20:07:40 -0500

    Queue Commit

---
+++ queue.py
@@ -0,0 +1,32 @@
+# This is a Python program to implement queue data structure
+
+class Queue:
+    def __init__(self):
+        self.items = []
+    
+    def isEmpty(self):
+        return self.items == []
+    
+    def enqueue(self, item):
+        self.items.insert(0, item)
+    
+    def dequeue(self, item):
+        self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+
+    def printQueue(self):
+        for item in self.items:
+            print(item)
+    
+object = Queue()
+print(object.isEmpty())
+object.enqueue(1)
+object.enqueue("UTA")
+object.printQueue()
+print(object.size())
+
+
+
+    
