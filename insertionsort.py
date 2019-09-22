ca492dd2791f0b6a1f7354b5a53f48777b99d656
Saroj Lamichhane <sarojlamichhane@Sarojs-MBP.attlocal.net>
2019-09-22 16:32:50 -0500

    Insertion Sort Commit

---
+++ insertionsort.py
@@ -0,0 +1,11 @@
+def insertionSort(nums):
+    for index in range(1, len(nums)):
+        number = nums[index]
+        i = index
+        while i > 0 and number < nums[i - 1]:
+            nums[i] = nums[i - 1]
+            i += -1
+        nums[i] = number
+    return nums
+
+print(insertionSort([5,1,9,0,8,2,6,2]))
